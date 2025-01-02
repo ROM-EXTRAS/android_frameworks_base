3420a7fd3a6b470c0f0f5f59a7fe6e6b7d7b7b98
droidfreak32 <rushabshah32@gmail.com>
2025-01-02 18:20:22 +0530

    SystemUI: Screenshot: Disable sounds
    Change-Id: I0b053ce921d6ff4c82b50bc4b29dbf8e3fb28a24

--- packages/SystemUI/src/com/android/systemui/screenshot/LegacyScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/LegacyScreenshotController.java
@@ -404,7 +404,7 @@ public class LegacyScreenshotController implements InteractiveScreenshotHandler 
 
     private void releaseMediaPlayer() {
         if (mScreenshotSoundController == null) return;
-        mScreenshotSoundController.releaseScreenshotSoundAsync();
+        // mScreenshotSoundController.releaseScreenshotSoundAsync();
     }
 
     /**
@@ -578,7 +578,7 @@ public class LegacyScreenshotController implements InteractiveScreenshotHandler 
     private void playCameraSoundIfNeeded() {
         if (mScreenshotSoundController == null) return;
         // the controller is not-null only on the default display controller
-        mScreenshotSoundController.playScreenshotSoundAsync();
+        // mScreenshotSoundController.playScreenshotSoundAsync();
     }
 
     /**
@@ -587,7 +587,7 @@ public class LegacyScreenshotController implements InteractiveScreenshotHandler 
      */
     private void saveScreenshotAndToast(ScreenshotData screenshot, Consumer<Uri> finisher) {
         // Play the shutter sound to notify that we've taken a screenshot
-        playCameraSoundIfNeeded();
+        // playCameraSoundIfNeeded();
 
         saveScreenshotInBackground(screenshot, UUID.randomUUID(), finisher, result -> {
             if (result.uri != null) {
