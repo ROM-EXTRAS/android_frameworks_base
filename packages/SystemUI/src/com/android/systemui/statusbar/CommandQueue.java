67431c1a7d0a7da408c2b52ea4b1483246601a81
droidfreak32 <rushabshah32@gmail.com>
2025-05-31 17:12:16 +0530

    fixup! Pulse remove references to leftInLandscape methods for now
     * Obsolete after https://github.com/lineageos/android_frameworks_base/commit/dbe3411a55945a99e152b27ce59b6bf1022157c4
    
    Change-Id: I1122e565bb2ccd12b8221b163bd32c96b04f5f92

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -184,7 +184,6 @@ public class CommandQueue extends IStatusBar.Stub implements
     private static final int MSG_SET_SPLITSCREEN_FOCUS = 81 << MSG_SHIFT;
     private static final int MSG_TOGGLE_QUICK_SETTINGS_PANEL = 82 << MSG_SHIFT;
     private static final int MSG_SCREEN_PINNING_STATE_CHANGED = 83 << MSG_SHIFT;
-    private static final int MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED = 84 << MSG_SHIFT;
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
     public static final int FLAG_EXCLUDE_RECENTS_PANEL = 1 << 1;
@@ -578,7 +577,6 @@ public class CommandQueue extends IStatusBar.Stub implements
         default void moveFocusedTaskToDesktop(int displayId) {}
 
         default void screenPinningStateChanged(boolean enabled) {}
-        default void leftInLandscapeChanged(boolean isLeft) {}
     }
 
     @VisibleForTesting
@@ -1506,15 +1504,6 @@ public class CommandQueue extends IStatusBar.Stub implements
         }
     }
 
-    @Override
-    public void leftInLandscapeChanged(boolean isLeft) {
-        synchronized (mLock) {
-            mHandler.removeMessages(MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED);
-            mHandler.obtainMessage(MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED,
-                    isLeft ? 1 : 0, 0, null).sendToTarget();
-        }
-    }
-
     private final class H extends Handler {
         private H(Looper l) {
             super(l);
@@ -2041,11 +2030,6 @@ public class CommandQueue extends IStatusBar.Stub implements
                 case MSG_SCREEN_PINNING_STATE_CHANGED:
                     for (int i = 0; i < mCallbacks.size(); i++) {
                         mCallbacks.get(i).screenPinningStateChanged(msg.arg1 != 0);
-                    }
-                    break;
-                case MSG_LEFT_IN_LANDSCAPE_STATE_CHANGED:
-                    for (int i = 0; i < mCallbacks.size(); i++) {
-                        mCallbacks.get(i).leftInLandscapeChanged(msg.arg1 != 0);
                     }
                     break;
             }
