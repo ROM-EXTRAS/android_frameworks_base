570f8c9764c4c4064b8883b05d52d3132eb0469e
droidfreak32 <rushabshah32@gmail.com>
2025-08-15 06:29:11 +0530

    Revert "base: treewide: Disable debug logging"
    This reverts commit 6bac39f3cd495f160e19a94b31aceb6efeb69c35.
    
    Change-Id: I8f825e2723e812280cc1c8c9540b29cf278a4d84

--- packages/SettingsLib/src/com/android/settingslib/bluetooth/HearingDeviceLocalDataManager.java
+++ packages/SettingsLib/src/com/android/settingslib/bluetooth/HearingDeviceLocalDataManager.java
@@ -51,7 +51,7 @@ import java.util.concurrent.Executor;
  */
 public class HearingDeviceLocalDataManager {
     private static final String TAG = "HearingDeviceDataMgr";
-    private static final boolean DEBUG = false;
+    private static final boolean DEBUG = true;
 
     /** Interface for listening hearing device local data changed */
     public interface OnDeviceLocalDataChangeListener {
